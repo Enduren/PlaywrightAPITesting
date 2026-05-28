e7d6eb69d1a0f6e9c49c878277d381fd2c81053d
dexter <dtennison79@gmail.com>
2026-05-28 11:29:04 -0500

    first commit

---
+++ tests/apiTest.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+
+
+test('simple get request', async ({ request }) => {
+  const response=   await request.get('https://conduit-api.bondaracademy.com/api/tags')
+  const responseObject= await response.json()
+  console.log(responseObject);
+  expect(responseObject.tags[0]).toEqual('Test')
+  expect(responseObject.tags).toHaveLength(10)
+});
+
+// test('simple POST request', async ({ request }) => {
+//   const response=   await request.post('https://conduit-api.bondaracademy.com/api/articles',{
+//     headers:{
+//       Authorization:''
+//     },
+//     data:{
+
+//     }
+
+//   })
+//   const responseObject= await response.json()
+//   console.log(responseObject);
+//   expect(responseObject.tags[0]).toEqual('Test')
+//   expect(responseObject.tags).toHaveLength(10)
+// });
+
