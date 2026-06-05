682b76dbe8ef399c98feb653a9a40fa3be09c069
dexter <dtennison79@gmail.com>
2026-06-05 10:40:16 -0500

    remove extra comments

--- tests/apiPutTest.spec.ts
+++ tests/apiPutTest.spec.ts
@@ -2,24 +2,6 @@ import { test, expect, request, APIRequestContext } from '@playwright/test';
 
 
 test.describe('API PUT Requests', () => {
-    let reqContext: APIRequestContext; 
-    
-    // test.beforeAll(async () => {
-    //     // 2. make url comon for all the tests
-    //     reqContext = await request.newContext({
-    //         baseURL: 'https://restful-booker.herokuapp.com',
-    //         extraHTTPHeaders: {
-    //             'Accept': 'application/json',
-    //             'Cookie': 'token=abc123',
-    //         },
-    //     });
-    // });
-
-    // // 3. Clean up the context after all tests finish
-    // test.afterAll(async () => {
-    //     await reqContext.dispose();
-    // });
-
 
     test('Uses the baseURL from the config file', async ({ request }) => {
         const response = await request.put('/booking/1', {
